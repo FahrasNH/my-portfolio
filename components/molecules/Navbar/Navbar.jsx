19307fa8133fa3123304ba6a0d48111dd109733b
Fahras Nur Hidayat <fahras1998@yahoo.com>
2021-01-04 19:12:28 +0700

    Navbar condition

--- components/molecules/Navbar/Navbar.jsx
+++ components/molecules/Navbar/Navbar.jsx
@@ -8,55 +8,59 @@ import { capitalize } from 'lodash'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
-const Navbar = ({ state, setState }) => (
-  <Grid container style={{ height: '10%' }}>
-    <Grid item xs={6}>
-      <View
-        backgroundImage="linear-gradient(to bottom right, #321D51, #5D3D8C)"
-        height="80px"
-        width="205px"
-        borderRadius="0 0 4px 0"
-      >
-        <Image
-          source={require('../../../assets/illustrations/Dot.svg')}
-          imageName="ic-dot"
-          position="absolute"
-          top="20px"
-          left="105px"
-        />
-      </View>
-    </Grid>
-    <Grid item xs={6} style={{
-      display: 'flex',
-      justifyContent: 'flex-end'
-    }}>
-      <List className="navbar-menu">
-        {['/', '/portfolio', '/contact'].map((text, index) => {
-          const router = useRouter()
-          const active = router.pathname === text ? 'active' : ''
+const Navbar = ({ state, setState }) => {
+  const router = useRouter()
 
-          return (
-            <ListItem button key={text} className="navbar-item">
-              <Link href={text}>
-                <ListItemText primary={text !== '/' ? capitalize(text.slice(1)) : 'Menu'} className={`navbar-text ${active}`}/>
-              </Link>
+  return (
+    <Grid container style={{ height: '10%' }}>
+      <Grid item xs={6}>
+        <View
+          backgroundImage="linear-gradient(to bottom right, #321D51, #5D3D8C)"
+          height="80px"
+          width="205px"
+          borderRadius="0 0 4px 0"
+        >
+          <Image
+            source={require('../../../assets/illustrations/Dot.svg')}
+            imageName="ic-dot"
+            position="absolute"
+            top="20px"
+            left="105px"
+          />
+        </View>
+      </Grid>
+      <Grid item xs={6} style={{
+        display: 'flex',
+        justifyContent: 'flex-end'
+      }}>
+        <List className="navbar-menu">
+          {['/', '/portfolio', '/contact'].map((text, index) => {
+            const active = router.pathname === text ? 'active' : ''
+
+            return (
+              <ListItem button key={text} className="navbar-item" style={{ marginRight: (router.pathname !== '/') && text === '/contact' ? '0' : '50px', }}>
+                <Link href={text}>
+                  <ListItemText primary={text !== '/' ? capitalize(text.slice(1)) : 'Menu'} className={`navbar-text ${active}`}/>
+                </Link>
+              </ListItem>
+            )
+          })}
+
+          { router.pathname === '/' && (
+            <ListItem className="navbar-switch">
+              <ListItemIcon>
+                <Switch
+                  name="switchMode"
+                  classNames="switch-mode"
+                  state={state}
+                  setState={setState}
+                />
+              </ListItemIcon>
             </ListItem>
-          )
-        })}
-
-        <ListItem className="navbar-switch">
-          <ListItemIcon>
-            <Switch
-              name="switchMode"
-              classNames="switch-mode"
-              state={state}
-              setState={setState}
-            />
-          </ListItemIcon>
-        </ListItem>
-      </List>
+          )}
+        </List>
+      </Grid>
     </Grid>
-  </Grid>
-)
-
+  )
+}
 export default  Navbar
